cb76b0a55a338fae796980e8b72375bc9e87e04a
JRaams <47593569+JRaams@users.noreply.github.com>
2021-08-27 11:40:57 +0200

    replace polling with rAF

--- purgewhitespace.js
+++ purgewhitespace.js
@@ -3,42 +3,54 @@ const isAvansD2l = loc.indexOf("/d2l/") > -1;
 const isBrightspaceCom = loc.indexOf("/apps/") > -1;
 const isAvansContent = loc.indexOf("/content/") > -1;
 
+const waitForElement = async (selector) => {
+  while (document.querySelector(selector) === null) {
+    await new Promise((resolve) => requestAnimationFrame(resolve));
+  }
+  return document.querySelector(selector);
+};
+
 // Base avans site -> https://brightspace.avans.nl/d2l/le/lessons/...
 if (isAvansD2l) {
   let navHeader = document
     .querySelector("d2l-navigation-main-header")
     .shadowRoot.querySelector(".d2l-navigation-centerer");
   navHeader.style.maxWidth = "initial";
-}
 
+  let navFooter = document
+    .querySelector("d2l-navigation-main-footer")
+    .shadowRoot.querySelector(".d2l-navigation-centerer");
+  navFooter.style.maxWidth = "initial";
+
+  const d2lPageMain = document.querySelector(".d2l-page-main");
+  d2lPageMain.style.maxWidth = "initial";
+
+  const leftCol = d2lPageMain.querySelector(
+    ".d2l-homepage .homepage-col-6:nth-child(1)"
+  );
+  leftCol.style.marginRight = "1em";
+}
 // First iframe, content container -> src="//s.brightspace.com/apps/smart-curriculum/..."
 if (isBrightspaceCom) {
-  let findRootWrapper = setInterval(function () {
-    let rootWrapper = document.querySelector("#root-wrapper");
-    if (rootWrapper) {
-      rootWrapper.style.maxWidth = "initial";
-      clearInterval(findRootWrapper);
-    }
-  }, 100);
+  waitForElement("#root-wrapper").then((rootWrapper) => {
+    rootWrapper.style.maxWidth = "initial";
+
+    const $moduleDisplay = rootWrapper.querySelector(".module-display");
+    $moduleDisplay.style.width = "95%";
+  });
 }
 
 // Second iframe, main content -> https://brightspace.avans.nl//content/enforced/...
 if (isAvansContent) {
-  let findContent = setInterval(function () {
-    let content = document.querySelector(".content");
-    if (content) {
-      let containerFluid = content.querySelector(".container-fluid");
-      containerFluid.style.maxWidth = "initial";
-      containerFluid.style.padding = "0";
+  waitForElement(".content .container-fluid").then((containerFluid) => {
+    containerFluid.style.maxWidth = "initial";
+    containerFluid.style.padding = "0";
 
-      let row = containerFluid.querySelector("main div.row");
-      row.style.maxWidth = "initial";
+    let row = containerFluid.querySelector("main div.row");
+    row.style.maxWidth = "initial";
 
-      let col = row.querySelector("div");
-      col.style.width = "initial";
-      col.style.margin = "1em";
-
-      clearInterval(findContent);
-    }
-  }, 100);
+    let col = row.querySelector("div");
+    col.style.width = "initial";
+    col.style.margin = "1em";
+  });
 }
